3f4a8c8a1fdfcbec780a83d14868f648936e3685
Darian Muellerchen <notknownlowkey@gmail.com>
2021-12-08 15:41:14 -0500

    - fixed the Help page.

--- app/src/main/java/com/code/darinmuellerchen/myrecipe/HelpActivity.java
+++ app/src/main/java/com/code/darinmuellerchen/myrecipe/HelpActivity.java
@@ -2,12 +2,19 @@ package com.code.darinmuellerchen.myrecipe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class HelpActivity extends AppCompatActivity {
     private TextView instructions;
+    private TextView header;
+    private Button btnShowVersion;
+    private Button btnShowAuthors;
+    private Button btnShowHelp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,7 +22,38 @@ public class HelpActivity extends AppCompatActivity {
         setContentView(R.layout.help_page);
 
         instructions = findViewById(R.id.txtInstructions);
+        header = findViewById(R.id.txtHelp);
+        btnShowAuthors = findViewById(R.id.btnShowAuthors);
+        btnShowHelp = findViewById(R.id.btnShowHelp);
+        btnShowVersion = findViewById(R.id.btnShowVersionNum);
+
+
         instructions.setMovementMethod(new ScrollingMovementMethod());
+
+        btnShowAuthors.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                header.setText(R.string.Authors);
+                instructions.setText(R.string.AuthorDisplay);
+            }
+        });
+
+        btnShowHelp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                header.setText(R.string.Help);
+                instructions.setText(R.string.Instructions);
+            }
+        });
+
+        btnShowVersion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                header.setText(R.string.Version);
+                instructions.setText(R.string.VersionDisplay);
+            }
+        });
+
 
     }
 
