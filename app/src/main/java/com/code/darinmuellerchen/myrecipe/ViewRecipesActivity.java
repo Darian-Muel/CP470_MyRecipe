c9ee1889dd59ff05caf64ac2b94d776c93dd44f8
edmundru <31968190+edmundru@users.noreply.github.com>
2021-12-08 21:21:33 -0500

    Update ViewRecipesActivity.java

--- app/src/main/java/com/code/darinmuellerchen/myrecipe/ViewRecipesActivity.java
+++ app/src/main/java/com/code/darinmuellerchen/myrecipe/ViewRecipesActivity.java
@@ -1,12 +1,16 @@
 package com.code.darinmuellerchen.myrecipe;
 
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.service.autofill.LuhnChecksumValidator;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class ViewRecipesActivity extends AppCompatActivity {
     protected static final String ACTIVITY_NAME = "ViewRecipesActivity";
@@ -15,6 +19,8 @@ public class ViewRecipesActivity extends AppCompatActivity {
     private Button btnLunch;
     private Button btnDinner;
     private Button btnOther;
+    BottomNavigationView bottomNavigationView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +30,29 @@ public class ViewRecipesActivity extends AppCompatActivity {
         btnLunch = findViewById(R.id.btnLunch);
         btnDinner = findViewById(R.id.btnDinner);
         btnOther = findViewById(R.id.btnOther);
+
+        bottomNavigationView = findViewById(R.id.bottom_navigator);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch(item.getItemId()){
+                    case R.id.create_navbar:
+                        startActivity(new Intent(getApplicationContext(), AddRecipesActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.home_navbar:
+                        startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.help_navbar:
+                        startActivity(new Intent(getApplicationContext(), HelpActivity.class));
+                        overridePendingTransition(0,0);
+                }
+                return false;
+            }
+        });
+
         btnFavourites.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
