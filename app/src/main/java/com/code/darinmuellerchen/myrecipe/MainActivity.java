cbd5cb011783f3e81969bece6ddf5c0c587356a8
Jordan <J4Y1450@hotmail.com>
2021-12-08 20:46:03 -0500

    Update MainActivity.java
    Added sign out code

--- app/src/main/java/com/code/darinmuellerchen/myrecipe/MainActivity.java
+++ app/src/main/java/com/code/darinmuellerchen/myrecipe/MainActivity.java
@@ -1,17 +1,29 @@
 package com.code.darinmuellerchen.myrecipe;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 public class MainActivity extends AppCompatActivity {
     protected static final String ACTIVITY_NAME = "MainActivity";
     private Button startViewRecipes;
     private Button startHelpPage;
     private Button startAddRecipes;
+    private Button signOut;
+
+    GoogleSignInClient mGoogleSignInClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +32,28 @@ public class MainActivity extends AppCompatActivity {
         startViewRecipes = findViewById(R.id.btnViewRecipes);
         startHelpPage = findViewById(R.id.btnHelp);
         startAddRecipes = findViewById(R.id.btnCreateRecipes);
+        signOut = findViewById(R.id.btnSignOut);
+
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        // Build a GoogleSignInClient with the options specified by gso.
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        //updateUI(account);
+
+        //Account information if we want to use it somewhere
+        GoogleSignInAccount acct = GoogleSignIn.getLastSignedInAccount(this);
+        if (acct != null) {
+            String personName = acct.getDisplayName();
+            String personGivenName = acct.getGivenName();
+            String personFamilyName = acct.getFamilyName();
+            String personEmail = acct.getEmail();
+            String personId = acct.getId();
+            Uri personPhoto = acct.getPhotoUrl();
+        }
 
 
         startViewRecipes.setOnClickListener(new View.OnClickListener() {
@@ -43,6 +77,27 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        signOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switch (view.getId()) {
+                    case R.id.btnSignOut:
+                        signOut();
+                        break;
+                }
+            }
+        });
 
+    }
+
+    private void signOut() {
+        mGoogleSignInClient.signOut()
+                .addOnCompleteListener(this, new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        //Toast.makeText(MainActivity.this, "Signed Out!", Toast.LENGTH_LONG).show();
+                        finish();
+                    }
+                });
     }
 }
