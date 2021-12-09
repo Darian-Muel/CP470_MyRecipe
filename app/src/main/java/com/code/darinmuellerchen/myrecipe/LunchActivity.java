f0d7d86561ec1dd35283bc4005cb319b87916734
edmundru <31968190+edmundru@users.noreply.github.com>
2021-12-08 21:20:35 -0500

    Update LunchActivity.java

--- app/src/main/java/com/code/darinmuellerchen/myrecipe/LunchActivity.java
+++ app/src/main/java/com/code/darinmuellerchen/myrecipe/LunchActivity.java
@@ -2,35 +2,38 @@ package com.code.darinmuellerchen.myrecipe;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.text.method.ScrollingMovementMethod;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class HelpActivity extends AppCompatActivity {
-    private TextView instructions;
-    private TextView header;
-    private Button btnShowVersion;
-    private Button btnShowAuthors;
-    private Button btnShowHelp;
+public class LunchActivity extends AppCompatActivity {
+
+    private Button btnDelete;
     BottomNavigationView bottomNavigationView;
+
+    RecyclerView recyclerView;
+    int n = RecipeLists.lunchList.size();
+    String[] s1 = new String[n];
+    String[] s2 = new String[n];
+    String[] s3 = new String[n];
+    Bitmap[] images = new Bitmap[n];
+    private MyAdapter.RecyclerViewCLickListener listener;
+    private MyAdapter myAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.help_page);
 
-        instructions = findViewById(R.id.txtInstructions);
-        header = findViewById(R.id.txtHelp);
-        btnShowAuthors = findViewById(R.id.btnShowAuthors);
-        btnShowHelp = findViewById(R.id.btnShowHelp);
-        btnShowVersion = findViewById(R.id.btnShowVersionNum);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lunch);
 
         bottomNavigationView = findViewById(R.id.bottom_navigator);
 
@@ -54,36 +57,49 @@ public class HelpActivity extends AppCompatActivity {
             }
         });
 
-        instructions.setMovementMethod(new ScrollingMovementMethod());
+        for (int i = 0; i < RecipeLists.lunchList.size();i++){
+            Recipe recipe = RecipeLists.lunchList.get(i);
+            s1[i] = recipe.getTitle();
+            s2[i] = recipe.getDescription();
+            s3[i] = recipe.getIngredients();
+            images[i] = recipe.getImageBitmap();
 
-        btnShowAuthors.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                header.setText(R.string.Authors);
-                instructions.setText(R.string.AuthorDisplay);
-            }
-        });
 
-        btnShowHelp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                header.setText(R.string.Help);
-                instructions.setText(R.string.Instructions);
-            }
-        });
+            //images[i] = R.drawable.turkey_food;
+        }
 
-        btnShowVersion.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                header.setText(R.string.Version);
-                instructions.setText(R.string.VersionDisplay);
-            }
-        });
-
+        setAdapter();
 
     }
 
+    private void setAdapter() {
 
+        FavouritesOnClickListener();
+        recyclerView = findViewById(R.id.recyclerViewFavourites);
+        myAdapter = new MyAdapter( this, s1, images, listener);
+        recyclerView.setAdapter(myAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
+    }
 
+    private void FavouritesOnClickListener() {
+        listener = new MyAdapter.RecyclerViewCLickListener() {
+            @Override
+            public void onClick(View v, int position) {
+                Intent intent = new Intent(getApplicationContext(), PopupActivity.class);
+                intent.putExtra("Title", RecipeLists.lunchList.get(position).getTitle());
+                intent.putExtra("Description", RecipeLists.lunchList.get(position).getDescription());
+                intent.putExtra("Picture", RecipeLists.lunchList.get(position).getImageBitmap());
+                intent.putExtra("Ingredients", RecipeLists.lunchList.get(position).getIngredients());
+                startActivity(intent);
+            }
+
+            @Override
+            public void onDeleteClick(int position) {
+                RecipeLists.lunchList.remove(position);
+                Intent intent = new Intent(LunchActivity.this, LunchActivity.class);
+                startActivity(intent);
+            }
+        };
+    }
 }
