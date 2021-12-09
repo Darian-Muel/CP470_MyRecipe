542eabc4783092e4a97a9e4e5f2ccbf87e890953
edmundru <31968190+edmundru@users.noreply.github.com>
2021-12-08 21:19:33 -0500

    Update HelpActivity.java

--- app/src/main/java/com/code/darinmuellerchen/myrecipe/HelpActivity.java
+++ app/src/main/java/com/code/darinmuellerchen/myrecipe/HelpActivity.java
@@ -1,13 +1,17 @@
 package com.code.darinmuellerchen.myrecipe;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class HelpActivity extends AppCompatActivity {
     private TextView instructions;
@@ -15,6 +19,7 @@ public class HelpActivity extends AppCompatActivity {
     private Button btnShowVersion;
     private Button btnShowAuthors;
     private Button btnShowHelp;
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +32,27 @@ public class HelpActivity extends AppCompatActivity {
         btnShowHelp = findViewById(R.id.btnShowHelp);
         btnShowVersion = findViewById(R.id.btnShowVersionNum);
 
+        bottomNavigationView = findViewById(R.id.bottom_navigator);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch(item.getItemId()){
+                    case R.id.create_navbar:
+                        startActivity(new Intent(getApplicationContext(), AddRecipesActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.home_navbar:
+                        startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.help_navbar:
+                        startActivity(new Intent(getApplicationContext(), HelpActivity.class));
+                        overridePendingTransition(0,0);
+                }
+                return false;
+            }
+        });
 
         instructions.setMovementMethod(new ScrollingMovementMethod());
 
